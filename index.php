2448daa5adcef5fd27cbcfbcd289e20308acd4c6
Leo <leonardmartirosyan95@gmail.com>
2024-11-02 14:57:10 +0400

    add base-controller

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+// !! This file has to be in the webhosting's root folder! Otherwise the web won't work !!
+
+/*
+ *       _____ _____ _____                _       _
+ *      |_   _/  __ \_   _|              (_)     | |
+ *        | | | /  \/ | |  ___  ___   ___ _  __ _| |
+ *        | | ||      | | / __|/ _ \ / __| |/ _` | |
+ *       _| |_| \__/\ | |_\__ \ (_) | (__| | (_| | |
+ *      |_____\_____/ |_(_)___/\___/ \___|_|\__,_|_|
+ *                   ___
+ *                  |  _|___ ___ ___
+ *                  |  _|  _| -_| -_|  LICENCE
+ *                  |_| |_| |___|___|
+ *
+ * IT NEWS  <>  PROGRAMMING  <>  HW & SW  <>  COMMUNITY
+ *
+ * This source code is part of online courses at IT social
+ * network WWW.ICT.SOCIAL
+ *
+ * Feel free to use it for whatever you want, modify it and share it but
+ * don't forget to keep this link in code.
+ *
+ * For more information visit http://www.ict.social/licences
+ */
+session_start();
+ 
+// Setting internal encoding for string functions
+mb_internal_encoding("UTF-8");
+
+// Callback for autoloading controllers and models
+function autoloadFunction($class)
+{
+	// Ends with the string "Controller" ?
+    if (preg_match('/Controller$/', $class))	
+        require("controllers/" . $class . ".php");
+    else
+        require("models/" . $class . ".php");
+}
+
+// Registers the callback
+spl_autoload_register("autoloadFunction");
